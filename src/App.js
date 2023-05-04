8e0cd50e983eadef00654455ee75cc7f2e890e32
miyu <75245305+miyu0301@users.noreply.github.com>
2023-05-03 21:13:08 -0700

    Get pokemon data and Output into screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from './utills/pokemon';
+import { getAllPokemon, getPokemon } from './utills/pokemon';
+import Card from './components/Card'
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
       const fetchPokemonData = async () => {
       // get all Pokemon datas
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      // get Pokemon detail data
+      loadPokemon(res.results);
+      setLoading(false);
     }
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    )
+    setPokemonData(_pokemonData);
+  };
+console.log(pokemonData);
+
   return (
     <div className="App">
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <>
+          <div className='pokemonCardContainer'></div>
+          {pokemonData.map((pokemon, i) => {
+            return <Card key={i} pokemon={pokemon}></Card>;
+          })}
+        </>
+      )}
     </div>
   );
 }
+
 
 export default App;
